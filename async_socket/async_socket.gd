ddefea18e5ec3828bd3f422977dc65662726e97f
Paulo Carbalone <psoreto@outlook.com>
2024-06-05 12:34:31 -0300

    rewritten, stable api

---
+++ async_socket/async_socket.gd
@@ -0,0 +1,144 @@
+class_name AsyncSocket
+extends Object
+## The AsyncSocket namespace provides asynchronous, single-threaded TCP networking functionality.
+
+
+class TcpClient extends RefCounted:
+	signal connected
+	signal disconnected
+
+
+	var _stream: StreamPeerTCP
+	var _connected: bool
+	var _expecting := -1
+	var _internal: Internal
+
+
+	## Create a TcpClient from a StreamPeerTCP.
+	static func from_stream(stream: StreamPeerTCP) -> TcpClient:
+		stream.set_no_delay(true)
+
+		var client := TcpClient.new()
+		client._stream = stream
+		client._connected = true
+		return client
+
+
+	func _init():
+		_stream = StreamPeerTCP.new()
+		_internal = Internal.new()
+
+
+	## Connects to a host at the specified address and port.
+	## [codeblock]
+	## var client := AsyncSocket.TcpClient.new()
+	## if await client.connect_to_host('127.0.0.1', 5000) != OK:
+	##     print('failed to connect')
+	## [/codeblock]
+	func connect_to_host(host: String, port: int) -> Error:
+		var result := _stream.connect_to_host(host, port)
+		if result == OK:
+			await connected
+			_stream.set_no_delay(true)
+		return result
+
+
+	## Disconnects from the host.
+	## [codeblock]await client.disconnect_from_host()[/codeblock]
+	func disconnect_from_host() -> void:
+		_stream.disconnect_from_host()
+		await disconnected
+
+
+	## Sends a buffer of data to the connected host.
+	func send(buf: PackedByteArray) -> Error:
+		var result := _stream.put_partial_data(buf)
+		if result[0] != OK:
+			disconnect_from_host()
+		return result[0]
+
+
+	## Receives data from the connected host.
+	## [codeblock]await client.recv(10)[/codeblock]
+	func recv(size: int) -> Array:
+		# Calling client.recv multiple times without await.
+		if _expecting != -1:
+			return [FAILED]
+
+		_expecting = size
+		return [OK, await _internal.done]
+
+
+	## Polls the TCP stream for updates on connection status and received data.
+	func poll() -> void:
+		_stream.poll()
+		var status := _stream.get_status()
+
+		match status:
+			StreamPeerTCP.Status.STATUS_CONNECTED:
+				if not _connected:
+					_connected = true
+					connected.emit()
+				if _expecting != -1 and _stream.get_available_bytes() >= _expecting:
+					var result := _stream.get_partial_data(_expecting)
+					if result[0] != OK:
+						disconnect_from_host()
+						return
+
+					_expecting = -1
+					_internal.done.emit(result[1])
+			StreamPeerTCP.Status.STATUS_NONE, StreamPeerTCP.Status.STATUS_ERROR:
+				if _connected:
+					_connected = false
+					disconnected.emit()
+
+
+class TcpListener extends RefCounted:
+	signal client_connected(client: TcpClient)
+	signal client_disconnected(client: TcpClient)
+
+
+	var _listener: TCPServer
+	var _host: String
+	var _port: int
+	var _clients: Array
+
+
+	func _init(host: String, port: int) -> void:
+		_host = host
+		_port = port
+		_listener = TCPServer.new()
+		_clients = []
+
+
+	## Starts listening for incoming TCP connections on the specified host and port.
+	func start() -> Error:
+		return _listener.listen(_port, _host)
+
+
+	## Stops listening for incoming TCP connections.
+	func stop() -> void:
+		_listener.stop()
+
+
+	func poll() -> void:
+		if _listener.is_connection_available():
+			var stream := _listener.take_connection()
+			var client := TcpClient.from_stream(stream)
+			client.disconnected.connect(func() -> void:
+				var idx := _clients.find(client)
+				var last_idx := _clients.size()
+				var temp = _clients[idx]
+				_clients[idx] = _clients[last_idx]
+				_clients[last_idx] = temp
+				_clients.resize(last_idx - 1)
+				client_disconnected.emit(client))
+			_clients.push_back(client)
+			client_connected.emit(client)
+
+		for c in _clients:
+			c.poll()
+
+
+class Internal extends Object:
+	signal done(something: Variant)
--- examples/test.gd
+++ examples/test.gd
@@ -1,60 +1,62 @@
 extends Node2D
 
 
-var _server: TcpListener
-var _client: TcpClient
+var _server: AsyncSocket.TcpListener
+var _client: AsyncSocket.TcpClient
 
 
 func _ready() -> void:
-    _handle_server()
-    _handle_client()
+	_handle_server()
+	_handle_client()
 
 
 func _handle_server() -> void:
-    _server = TcpListener.new(5000)
-    _server.start()
-
-    var result := await _server.accept() as Array
-    if result[0] != OK:
-        push_error('Failed to accept connection.')
-        # TcpListener.accept can only fail if the server forcefully shuts down
-        # while an AcceptTask is still pending. So we can safely return here.
-        return
-
-    var client := result[1] as TcpClient
-    print('S> client connected')
-
-    var buff := 'hello world'.to_ascii_buffer()
-    print('S> %d bytes sent!' % buff.size())
-    client.send(buff)
-
-    await get_tree().create_timer(1).timeout
-    client.disconnect_from_host()
+	_server = AsyncSocket.TcpListener.new('0.0.0.0', 5000)
+	_server.client_connected.connect(func(client: AsyncSocket.TcpClient) -> void:
+		print('S> client connected')
+		var expected := 'hello world'.to_ascii_buffer()
+		print('S> expecting message')
+		var result := await client.recv(expected.size()) as Array
+		if result[0] != OK:
+			print('S> failed to receive data from client')
+			return
+		print('S> message received: %s' % (result[1] as PackedByteArray).get_string_from_ascii())
+		_client.send(result[1])
+		print('S> server response sent')
+		print('S> waiting 1s before disconnecting client')
+		await get_tree().create_timer(1).timeout
+		_client.disconnect_from_host())
+	_server.client_disconnected.connect(func() -> void:
+		print('S> client disconnected')
+		_server.stop())
+	_server.start()
 
 
 func _handle_client() -> void:
-    _client = TcpClient.new()
-    _client.timeout = 1
-    _client.connect_to_host('127.0.0.1', 5000)
-
-    await _client.connected
-    print('C> connected')
-
-    var expected := 'hello world'.to_ascii_buffer()
-    var result := await _client.recv(expected.size()) as Array
-
-    if result[0] == OK:
-        print('C> %d bytes received: %s' % [result[1].size(), result[1].get_string_from_ascii()])
-    else:
-        push_error('Failed to receive message.')
-
-    await _client.disconnected
-    print('C> disconnected')
+	_client = AsyncSocket.TcpClient.new()
+	_client.connected.connect(func() -> void:
+		print('C> connected')
+		var message := 'hello world'.to_ascii_buffer()
+		print('C> waiting 1s before sending message')
+		await get_tree().create_timer(1).timeout
+		if _client.send(message) != OK:
+			print('C> failed to send hello')
+			return
+		print('C> message sent')
+		print('C> expecting response')
+		var result := await _client.recv(message.size()) as Array
+		if result[0] != OK:
+			print('C> failed to receive hello response from server')
+			return
+		print('C> got response: %s' % (result[1] as PackedByteArray).get_string_from_ascii()))
+	_client.disconnected.connect(func() -> void:
+		print('C> disconnected'))
+	await _client.connect_to_host('127.0.0.1', 5000)
 
 
 func _process(_delta: float) -> void:
-    if _client != null:
-        _client.poll()
+	if _client != null:
+		_client.poll()
 
-    if _server != null:
-        _server.poll()
+	if _server != null:
+		_server.poll()
